26600e1554f806fa506c44ebb5baacde6ddd51dc
Temirlan Kamal <kamaltemirlan@gmail.com>
2023-02-01 17:49:13 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const isChecked = true,
+      isClose = true;
+
+console.log(isChecked && isClose); // && - и  // работает только когда все аргументы правдивые
+console.log(isChecked || isClose); // || - или // работает если хотя бы один аргумент правдивый
+
+// ! - оператор отрецания "делает все наоборот" если true => false , если false => true
+
